1e92df7f7630a264e43737e612a02a77c229e556
Oliver Shih <olivershih@gmail.com>
2021-05-14 12:20:28 -0700

    cleaned up table data

--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -20,7 +20,14 @@ class SearchResultContainer extends Component {
     let results = [];
     await API.search()
       .then((res) => {
-        results = res.data.results;
+        results = res.data.results.map(result => {
+          return {
+            first: result.name.first,
+            last: result.name.last,
+            email: result.email,
+            date: result.dob.date,
+            picture: result.picture.large
+        }});
       })
       .catch((err) => console.log(err));
       return results;
@@ -29,10 +36,10 @@ class SearchResultContainer extends Component {
   handleInputChange = async (event) => {
     const value = event.target.value;
     const employees = await this.state.results.filter(employee => 
-      employee.name.first.toLowerCase().includes(value.toLowerCase()) 
-      || employee.name.last.toLowerCase().includes(value.toLowerCase()) 
-      || employee.email.toLowerCase().includes(value.toLowerCase()) 
-      || employee.dob.date.includes(value.toLowerCase())
+      employee.first.toLowerCase().includes(value.toLowerCase()) 
+      || employee.last.toLowerCase().includes(value.toLowerCase()) 
+      || employee.toLowerCase().includes(value.toLowerCase()) 
+      || employee.date.includes(value.toLowerCase())
       );
     await this.setState({
       search: value,
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,9 @@
 import React from "react";
+// import { MDBDataTable } from 'mdbreact';
 const moment = require("moment");
 
 function Table(props) {
   const employees = props.results;
-
   return (
       <table className="table align-middle table-primary table-hover table-striped table-bordered" id="sortTable">
         <thead>
@@ -17,18 +17,18 @@ function Table(props) {
         </thead>
         <tbody>
           {employees.map(result => (
-            <tr key={`${result.name.first} ${result.name.last} ${result.dob.date}`}>
+            <tr key={`${result.first} ${result.last} ${result.date}`}>
               <td className="d-flex justify-content-center">
                 <img
-                  alt={`${result.name.first} ${result.name.last}`}
-                  src={result.picture.large}
+                  alt={`${result.first} ${result.last}`}
+                  src={result.picture}
                   className="rounded-circle"
                 />
               </td>
-              <td>{result.name.first}</td>
-              <td>{result.name.last}</td>
+              <td>{result.first}</td>
+              <td>{result.last}</td>
               <td>{result.email}</td>
-              <td>{moment(result.dob.date).format("MM-DD-YYYY")}</td>
+              <td>{moment(result.date).format("MM-DD-YYYY")}</td>
             </tr>
           ))} 
         </tbody>
